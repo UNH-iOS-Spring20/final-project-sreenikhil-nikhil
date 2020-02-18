5f9f854e35baa7155b827ae0eb49729e0d9814ea
Sreenikhil <kollu1996@gmail.com>
2020-02-18 17:43:17 -0500

    Initial Commit

---
+++ Line Up/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Line Up
+//
+//  Created by Sreenikhil Kollu on 2/18/20.
+//  Copyright © 2020 Sreenikhil Kollu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Line UpTests/Line_UpTests.swift
@@ -0,0 +1,34 @@
+//
+//  Line_UpTests.swift
+//  Line UpTests
+//
+//  Created by Sreenikhil Kollu on 2/18/20.
+//  Copyright © 2020 Sreenikhil Kollu. All rights reserved.
+//
+
+import XCTest
+@testable import Line_Up
+
+class Line_UpTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
