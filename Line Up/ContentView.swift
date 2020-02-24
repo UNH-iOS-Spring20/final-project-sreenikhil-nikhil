ec136f6ea260ed37260b7db6717d46979a37ecf2
Sreenikhil <kollu1996@gmail.com>
2020-02-23 21:49:02 -0500

    Adding firebase setup and cocoapods

--- Line Up/ContentView.swift
+++ Line Up/ContentView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-    }
+       Text("Hello, World!")
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ Pods/Target Support Files/Pods-Line Up/Pods-Line Up-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Line_Up : NSObject
+@end
+@implementation PodsDummy_Pods_Line_Up
+@end
